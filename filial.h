a7026a1e6060c679a74f73895272d3d5fdeaf20a
kH3nrique <92551677+kH3nrique@users.noreply.github.com>
2023-08-24 11:32:20 -0300

    s

---
+++ filial.h
@@ -0,0 +1,23 @@
+#include "index.h"
+
+void InseriFilial()
+{
+    string filial;
+}
+
+void menuLogistica()
+{
+    int n;
+    cout << "1. Inserir filial" <<endl;
+    cout << "2. Remover filial" <<endl;
+        cin >> n;
+    switch (n)
+    {
+    case 1:
+        
+        break;
+    
+    default:
+        break;
+    }
+}
