4e6346bf8f693819e6220c6eaec1951b717f2999
kH3nrique <gabrielmartinssousa@gmail.com>
2023-09-20 09:16:52 -0300

    filial

--- filial.h
+++ filial.h
@@ -11,14 +11,17 @@ using namespace std;
 
 struct Filial
 {
-    int nFiliais = 0;
-    char locais[MAX_LOCATIONS][MAX_LOCATIONS];
-    int custo[MAX_LOCATIONS][MAX_LOCATIONS];//matrizAdjacencia
+    int andaFiliais = 0;
+    char filiais[MAX_LOCATIONS][MAX_LOCATIONS];
+    int andaCustos = 0;
+    int custo[MAX_LOCATIONS];//matrizAdjacencia
+    int grafo[MAX_LOCATIONS];
+    int andaGrafo = 0;
 };
 
 bool localExiste(const struct Filial* filial, const char* nomeLocal) {
-    for (int i = 0; i < filial->nFiliais; ++i) {
-        if (strcmp(filial->locais[i], nomeLocal) == 0) {
+    for (int i = 0; i < filial->andaFiliais; ++i) {
+        if (strcmp(filial->filiais[i], nomeLocal) == 0) {
             return true;
         }
     }
@@ -27,9 +30,9 @@ bool localExiste(const struct Filial* filial, const char* nomeLocal) {
 
 void insereFilial(struct Filial* filial, const char* cidade)
 {
-    // Numero de locais atingidos?
-    if (filial->nFiliais >= MAX_LOCATIONS) { 
-        cout << "Limite de locais atingido!!!" << endl;
+    // Numero de filiais atingidos?
+    if (filial->andaFiliais >= MAX_LOCATIONS) { 
+        cout << "Limite de filiais atingido!!!" << endl;
         return;
     }
 
@@ -39,21 +42,30 @@ void insereFilial(struct Filial* filial, const char* cidade)
         return;
     }
 
-    // Copia o nome do novo local para a matriz de locais no filial
-    strcpy(filial->locais[filial->nFiliais], cidade);
-    cout << "Filial '" << filial->locais[filial->nFiliais] << "' inserido com sucesso." << endl;
-    filial->nFiliais++;
+    // Copia o nome do novo local para a matriz de filiais no filial
+    strcpy(filial->filiais[filial->andaFiliais], cidade);
+    cout << "Filial '" << filial->filiais[filial->andaFiliais] << "' inserido com sucesso." << endl;
+    filial->andaFiliais++;
 }
 
 void insereMovimentacao(struct Filial* filial, const char* filial_um, const char* filial_dois)
 {
     int i1 = -1, i2 = -1;
-    for (int i = 0; i < filial->nFiliais; i++)
+    for (int i = 0; i < filial->andaFiliais; i++)
     {
-        if (strcmp(filial->locais[i], filial_um))
+        if (strcmp(filial->filiais[i], filial_um))
+        {
             i1 = i;
-        if (strcmp(filial->locais[i], filial_dois))
+            filial->grafo[filial->andaGrafo] = i;
+            filial->andaGrafo++;
+        }
+            
+        if (strcmp(filial->filiais[i], filial_dois))
+        {
             i2 = i;
+            filial->grafo[filial->andaGrafo] = i;
+            filial->andaGrafo++;
+        }
     }
     if (i1 == -1 || i2 == -1)
     {
@@ -61,28 +73,68 @@ void insereMovimentacao(struct Filial* filial, const char* filial_um, const char
         return;
     }
     cout << "Custo da viagem entre filiais: ";
-        cin >> filial->custo[i1][i2];
-    filial->custo[i2][i1] = filial->custo[i1][i2];
+        cin >> filial->custo[filial->andaCustos];
+        filial->andaCustos++;
 }
 
 void listaFiliaisProximas(struct Filial* filial, const char* pFilial)
 {
-    if (localExiste(filial, pFilial))
+    if (!localExiste(filial, pFilial))
     {
         cout << "Filial nao cadastrada!"<<endl;
         return;
     }
     
-    for (int i = 0; i < filial->nFiliais; i++)
+    for (int i = 0; i < filial->andaGrafo; i = i+2)
     {
-        
+        if (strcmp(filial->filiais[i], pFilial) == 0 || strcmp(filial->filiais[i+1], pFilial) == 0)
+            cout << "As filias, "<< filial->filiais[i] << " e " << filial->filiais[i+1] << " estao proximas"<<endl;
+    }
+}
+
+void atualizaMovimentacao(struct Filial* filial, const char* filial_um, const char* filial_dois)
+{
+    int i1 = -1, i2 = -1;
+    for (int i = 0; i < filial->andaFiliais; i++)
+    {
+        if (strcmp(filial->filiais[i], filial_um))
+        {
+            i1 = i;
+        }
+            
+        if (strcmp(filial->filiais[i], filial_dois))
+        {
+            i2 = i;
+        }
+    }
+    if (i1 == -1 || i2 == -1)
+    {
+        cout << "Filial(is) não encontrada(s)" <<endl;
+        return;
+    }
+    for (int i = 0; i < filial->andaGrafo; i = i+2)
+    {
+        if (strcmp(filial->filiais[i], filial_um) == 0 || strcmp(filial->filiais[i+1], filial_um) == 0)
+        {
+        }
+        if (strcmp(filial->filiais[i], filial_dois) == 0 || strcmp(filial->filiais[i+1], filial_dois) == 0)
+        {
+        }
     }
     
-    /*
-    após o usuário escolher uma filial hipotética i, o programa deverá listar todas as filiais j
-    para as quais exista alguma movimentação (i,j) de mercadoria cadastrada, inclusive com os custos de
-    movimentação para cada uma delas;
-    */
+}
+
+void removeFilial(struct Filial* filial, const char* eFilial)
+{
+    int i1 = -1, i2 = -1;
+    for (int i = 0; i < filial->andaFiliais; i++)
+    {
+        if (strcmp(filial->filiais[i], eFilial))
+        {
+            i1 = i;
+        }
+    }
+
 }
 
 void executarMenuFilial(struct Filial* filial)
@@ -96,7 +148,8 @@ void executarMenuFilial(struct Filial* filial)
         cout << "2. Insere movimentacao"<<endl;
         cout << "3. Lista filiais proximas"<<endl;
         cout << "4. Atualiza movimentacao"<<endl;
-        cout << "5. Voltar para o menu principal"<<endl;
+        cout << "5. Remover filial"<<endl;
+        cout << "6. Voltar para o menu principal"<<endl;
         cout << "0. Sair do programa"<<endl;
         cout << "Insira sua escolha: ";
             cin >> op;
@@ -125,7 +178,20 @@ void executarMenuFilial(struct Filial* filial)
                 insereMovimentacao(filial, cfilial1, cfilail2);
             break;
         case 3:
-            
+            char pFilial[100];
+            cout << "Digite a cidade da filial: ";
+                cin >> pFilial;
+            listaFiliaisProximas(filial, pFilial);
+            break;
+        case 4:
+
+            break;
+        case 5:
+            char eFilial[100];
+            cout <<"Digite a cidades das duas filiais:"<<endl;
+                cin >> cfilial1;
+                cin >> cfilail2;
+            removeFilial(filial, eFilial);
             break;
         default:
             break;
